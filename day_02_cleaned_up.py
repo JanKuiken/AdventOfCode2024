d6e81f6e66f7d9c511d8bcc91bf9752862aac0a1
Jan Kuiken <jan.kuiken@quicknet.nl>
2024-12-02 21:10:44 +0100

    renamed some things

--- day_02_cleaned_up.py
+++ day_02_cleaned_up.py
@@ -11,7 +11,7 @@ for line in lines:
     levels = [int(i) for i in line.split()]
     line_levels.append(levels)
 
-def check_levels_org(levels):
+def check_levels_part_1(levels):
     steps = [levels[i+1]-levels[i] for i in range(len(levels)-1)]
     for step in steps:
         if    abs(step) > 3                          \
@@ -20,22 +20,22 @@ def check_levels_org(levels):
             return False
     return True
 
-def check_levels_with_dampener(levels):
-    if check_levels_org(levels):
+def check_levels_part_2(levels):
+    if check_levels_part_1(levels):
         return True
     for i in range(len(levels)):
         levels_copy = levels.copy()
         del levels_copy[i]
-        if check_levels_org(levels_copy):
+        if check_levels_part_1(levels_copy):
             return True
     return False
 
 n_save_part_1 = 0
 n_save_part_2 = 0
 for levels in line_levels:
-    if check_levels_org(levels):
+    if check_levels_part_1(levels):
         n_save_part_1 += 1
-    if check_levels_with_dampener(levels):
+    if check_levels_part_2(levels):
         n_save_part_2 += 1
 
 print("Answer part 1 : ", n_save_part_1)
