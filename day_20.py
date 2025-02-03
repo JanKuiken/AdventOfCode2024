077bc58e02c4c923ac358d273858054adef40ca7
Jan Kuiken <jan.kuiken@quicknet.nl>
2025-02-03 19:32:46 +0100

    using TablePoint.iterate and added speedup function

--- day_20.py
+++ day_20.py
@@ -31,40 +31,34 @@ if False:
 ###############""".split('\n')
     matrix =  [ [ c for c in line ] for line in lines ]
 
-n_rows = len(matrix)
-n_cols = len(matrix[0])
-
-aoc.TablePoint.max_row = n_rows
-aoc.TablePoint.max_col = n_cols
+aoc.TablePoint.max_row = len(matrix)
+aoc.TablePoint.max_col = len(matrix[0])
 
 original_matrix = deepcopy(matrix)
 aoc.print_matrix(matrix)
 
 start = None
 end   = None
-for row in range(n_rows):
-    for col in range(n_cols):
-        if matrix[row][col] == 'S': start = aoc.TablePoint(row,col)
-        if matrix[row][col] == 'E': end   = aoc.TablePoint(row,col)
+for tp in aoc.TablePoint.iterate():
+    if matrix[tp.row][tp.col] == 'S': start = aoc.TablePoint(tp.row,tp.col)
+    if matrix[tp.row][tp.col] == 'E': end   = aoc.TablePoint(tp.row,tp.col)
 print('start', start)
 print('end', end)
 
 #create a graph and determine start and end point
 def create_graph():
     graph = defaultdict(set)
-    for row in range(n_rows):
-        for col in range(n_cols):
-            tp1 = aoc.TablePoint(row,col)
-            if matrix[row][col] in '.SE':
-                for tp2 in tp1.cartesian_neighbours():
-                    if matrix[tp2.row][tp2.col] in '.SE':
-                        graph[tp1].add(tp2)
-                        graph[tp2].add(tp1)
+    for tp1 in aoc.TablePoint.iterate():
+        if matrix[tp1.row][tp1.col] in '.SE':
+            for tp2 in tp1.cartesian_neighbours():
+                if matrix[tp2.row][tp2.col] in '.SE':
+                    graph[tp1].add(tp2)
+                    graph[tp2].add(tp1)
     return graph
 
 A_LOT = 9999999999
-start_to_end = [ [ A_LOT for _ in range(n_cols) ] for _ in range(n_rows) ]
-end_to_start = [ [ A_LOT for _ in range(n_cols) ] for _ in range(n_rows) ]
+start_to_end = [ [ A_LOT for _ in range(aoc.TablePoint.max_col) ] for _ in range(aoc.TablePoint.max_row) ]
+end_to_start = [ [ A_LOT for _ in range(aoc.TablePoint.max_col) ] for _ in range(aoc.TablePoint.max_row) ]
 
 def bfs(graph, start, matrix_to_fill):  # see https://en.wikipedia.org/wiki/Breadth-first_search
     Q = deque()
@@ -98,13 +92,18 @@ def cheat_result(row,col):
 
 print('initial_result', initial_result)
 
+# take care we change the limits of our TablePoint class
+aoc.TablePoint.min_row = 1
+aoc.TablePoint.min_col = 1
+aoc.TablePoint.max_row = len(matrix) - 1
+aoc.TablePoint.max_col = len(matrix[0]) - 1
+
 results = {}
-for row in range(1, n_rows-1):
-    for col in range(1, n_cols-1):
-        if matrix[row][col] == '#':
-            r = cheat_result(row,col)
-            if r <= initial_result - saving_picoseconds_part_1:
-                results[(row,col)] = r
+for tp in aoc.TablePoint.iterate():
+    if matrix[tp.row][tp.col] == '#':
+        r = cheat_result(tp.row, tp.col)
+        if r <= initial_result - saving_picoseconds_part_1:
+            results[tp] = r
 
 print("Answer part 1 : ", len(results))
 
@@ -119,29 +118,32 @@ print("Answer part 1 : ", len(results))
 #     routes do not count, they are 'the same cheat'
 # - there are no muliple cheats
 
-def distance(row1, col1, row2, col2):
-    return abs(row1 - row2) + abs(col1 - col2)
+def distance(tp1, tp2):
+    return abs(tp1.row - tp2.row) + abs(tp1.col - tp2.col)
 
 # for conviniece (we dont have to check for 'S' and 'E')
 matrix[start.row][start.col] = '.'
 matrix[end.row][end.col] = '.'
 
-def cheat_result_2(row1, col1, row2, col2):
-    return start_to_end[row1][col1] + end_to_start[row2][col2] 
+def cheat_result_2(tp1, tp2):
+    return start_to_end[tp1.row][tp1.col] + end_to_start[tp2.row][tp2.col] 
 
-# we need an aweful lots of idents....
+def points_within_manhatan_distance(tp, dist):
+    for drow in range(-dist, dist+1):
+        for dcol in range(-(dist - abs(drow)), (dist - abs(drow))+1):
+            tp2 = aoc.TablePoint(tp.row + drow, tp.col + dcol)
+            if tp2.isInbounds():
+                yield(tp2, abs(drow) + abs(dcol))
+
 results = {}
-for row1 in range(1, n_rows-1):
-    for col1 in range(1, n_cols-1):
-        if matrix[row1][col1] == '.':
-            for row2 in range(1, n_rows-1):
-                for col2 in range(1, n_cols-1):
-                    if matrix[row2][col2] == '.':
-                        dist = distance(row1, col1, row2, col2)
-                        if dist <= 20:
-                            r = cheat_result_2(row1, col1, row2, col2) + dist 
-                            if r <= initial_result - saving_picoseconds_part_2:
-                                results[(row1, col1, row2, col2, dist)] = r
+distance = 20
+for tp1 in aoc.TablePoint.iterate():
+    if matrix[tp1.row][tp1.col] == '.':
+        for tp2, dist in points_within_manhatan_distance(tp1, distance):
+            if matrix[tp2.row][tp2.col] == '.':
+                r = cheat_result_2(tp1, tp2) + dist
+                if r <= initial_result - saving_picoseconds_part_2:
+                    results[(tp1, tp2, dist)] = r
 
 print('saving_picoseconds_part_2',saving_picoseconds_part_2)
 
